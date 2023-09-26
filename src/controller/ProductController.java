ec3c343600ca07c1b6402abae07af3f0281cdf0d
AndreyK1993 <andrey.kurilenko27@gmail.com>
2023-09-26 22:09:10 +0300

    finish 1

--- src/controller/ProductController.java
+++ src/controller/ProductController.java
@@ -13,7 +13,7 @@ public class ProductController {
         double winnings = model.calculateWinnings();
         String winningsFormatted = Rounder.roundValue(winnings);
 
-        view.getOutput(winningsFormatted);
+        //view.getOutput(value, winningsFormatted, random) ;
 
         System.out.println("Value: " + value);
         System.out.println("Your winnings: " + winningsFormatted);
--- src/view/ProductView.java
+++ src/view/ProductView.java
@@ -1,16 +1,7 @@
 package view;
 
-//import java.util.Scanner;
 
 public class ProductView {
-
-    //public double[] getData() {
-
-        //Scanner sc = new Scanner(System.in);
-        //System.out.print("Enter value: ");
-        //String value = sc.nextLine().trim();
-        //return new double [] {value};
-    //}
 
     public void getOutput(String output) {
 
