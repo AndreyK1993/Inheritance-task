449f28a156614c04623163bb19942da8b9656f29
AndreyK1993 <andrey.kurilenko27@gmail.com>
2023-09-26 21:59:30 +0300

    Change 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import controller.ProductController;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         int random = (int) (Math.random() * 10);
 
-        double value = 10;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value: ");
+        double value = Double.parseDouble(sc.nextLine().trim());
+
 
         ProductController controller = new ProductController();
 
